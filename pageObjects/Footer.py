228eb1fe5f6e47dba950a6bfe276f7b55e0b145f
MaheshKharat297 <mahii297@gmail.com>
2023-09-10 20:30:07 +0530

    Changed Footer Object

--- pageObjects/Footer.py
+++ pageObjects/Footer.py
@@ -9,17 +9,15 @@ class VerifyFooter:
         self.driver = driver
 
     def checkPowerBy(self):
-        flag = False
         power_statement = self.driver.find_element(By.XPATH, self.lnkPowerVy_xpath).text
         print(power_statement)
         if power_statement == "nopCommerce":
-            flag = True
+            return True
 
     def checkVersion(self):
-        flag = False
         version_no = self.driver.find_element(By.XPATH, self.txtVersion_xpath).text
         if version_no == "nopCommerce version 4.60.0":
-            flag = True
+            return True
 
 
 
